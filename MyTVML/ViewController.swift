b884e73b9bca00d295c8261be9abd82e9b6bf7ec
Anand.sw <anand.sw@ZL-BLR-MAC071.local>
2021-12-08 11:55:02 +0530

    initial files

---
+++ MyTVML/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyTVML
+//
+//  Created by Anand.sw on 08/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonAction(_ sender: Any) {
+        print("Hi TV")
+    }
+    
+}
+
