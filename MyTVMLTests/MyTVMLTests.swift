df5efe46336afa96086fd9d5fe81f54c74f28f20
Anand.sw <anand.sw@ZL-BLR-MAC071.local>
2021-12-08 16:10:48 +0530

    added Gemfile & Dangerfile

--- MyTVMLTests/MyTVMLTests.swift
+++ MyTVMLTests/MyTVMLTests.swift
@@ -21,15 +21,8 @@ class MyTVMLTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let value = false
+        let value = true
         XCTAssert(value)
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
